db1e9ba9207c7b578c5242de3cf27f3694b84f1f
Jonathan  Allison <56617776+qb1968@users.noreply.github.com>
2020-01-30 13:57:41 -0500

    Add files via upload

---
+++ build/precache-manifest.a48f6a9701e88fdfa3e87c78fba05270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c5667388003dda5dd61aff029765d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21501eb0356074e20d13",
+    "url": "/static/css/main.536bcb13.chunk.css"
+  },
+  {
+    "revision": "bb808929955ab90bcb98",
+    "url": "/static/js/2.ef27278c.chunk.js"
+  },
+  {
+    "revision": "21501eb0356074e20d13",
+    "url": "/static/js/main.87bf503e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86ba3c825ee4de32078d1559cd839974",
+    "url": "/static/media/The_Smurfs_Song.86ba3c82.mp3"
+  }
+]);
